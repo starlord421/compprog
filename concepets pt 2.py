172ae9bb0a5a25762268a15abd9e4b4155da2fc3
Scholar <scholar@paradigm.local>
2023-09-26 12:53:06 -0600

    input thing

---
+++ concepets pt 2.py
@@ -0,0 +1,5 @@
+for i in range (50):
+    if i >24:
+        break
+    else:
+        print(i)
---
+++ input_thing.py
@@ -0,0 +1,32 @@
+print('congrats, the company has deemed that a five year employee is to recive a bonus of 5%')
+
+salary = int(input('what is your salary'))
+
+year = int(input('how long have you been with us'))
+
+if year >= 5: print (salary * 1.05 )
+else: print ('you are not qualified')
+
+square_vs_rectangle_length = input('what demention do you want')
+
+square_vs_rectangle_side=input('what is the second demention')
+
+if square_vs_rectangle_side == square_vs_rectangle_length: print ('this is a square')
+
+else: print ('this is a rectangle')
+
+user_input = int(input('what number do you want'))
+
+twoser_input = int(input('what other number do you want'))
+
+if user_input > twoser_input: print(user_input)
+
+else: print(twoser_input)
+
+attendence = int(input('what percentage of class attendence do you have'))
+
+if attendence >= 75: print ('you are free to take the exam')
+
+else: print ('then get out of here')
+
+if attendence > 100: print ('nice try fool, get the frick out of here.')
