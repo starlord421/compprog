1bbce787bcd3d3966532a8d83bb462ff5e3c3a08
Scholar <scholar@paradigm.local>
2023-10-12 13:04:41 -0600

    screwyouhunt

---
+++ morse.py
@@ -0,0 +1,80 @@
+a = ".-"
+b= "-..."
+c="-.-."
+d="-.."
+e="."
+f="..-."
+g="--."
+h="...."
+li=".."
+j=".---"
+k="-.-"
+l=".-.."
+m="--"
+n="-."
+o="---"
+p=".--."
+q="--.-"
+r=".-."
+s="..."
+t="-"
+u="..-"
+v="...-"
+w=".--"
+x="-..-"
+y="-.--"
+z="--.."
+morse=input("what message do you want to translate")
+for i in morse:
+    if i == 'a':
+        print (a)
+    if i == 'b':
+        print (b)
+    if i == 'c':
+        print (c)
+    if i == 'd':
+        print (d)
+    if i == 'e':
+        print (e)
+    if i == 'f':
+        print (f)
+    if i == 'g':
+        print (g)
+    if i == 'h':
+        print (h)
+    if i == 'i':
+        print (li)
+    if i == 'j':
+        print (j)
+    if i == 'k':
+        print (k)
+    if i == 'l':
+        print (l)
+    if i == 'm':
+        print (m)
+    if i == 'n':
+        print (n)
+    if i == 'o':
+        print (o)
+    if i == 'p':
+        print (p)
+    if i == 'q':
+        print (q)
+    if i == 'r':
+        print (r)
+    if i == 's':
+        print (s)
+    if i == 't':
+        print (t)
+    if i == 'u':
+        print (a)
+    if i == 'v':
+        print (v)
+    if i == 'w':
+        print (w)
+    if i == 'x':
+        print (a)
+    if i == 'y':
+        print (y)
+    if i == 'z':
+        print (z)
