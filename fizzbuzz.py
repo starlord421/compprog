65cf43521632f0bd52e0c5a7e1dae1cd5770cb32
starlord421 <143746579+starlord421@users.noreply.github.com>
2023-10-20 10:34:09 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 fizzbuzz=input("what number do you want to buzz")
 for i in range(0,int(fizzbuzz)):
+    if i % 3 != 0 or i % 2 != 0 or i % 5 != 0:
+        print("prime")
     if i % 3== 0 and i % 5== 0:
         print("fizzbuzz")
     elif i % 3==0:
