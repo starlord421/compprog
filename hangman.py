f06fd7b9cea46da9c2375e6bfb2fa879aeea2c16
Scholar <scholar@paradigm.local>
2023-11-28 13:04:49 -0700

    hangman

--- hangman.py
+++ hangman.py
@@ -1,19 +1,19 @@
 print("welcome to hangman")
 word="nerd"
 guessed_letters=[]
-
+guesses=1
 def desplay_clue(word, guessed_letters):
     clue=""
     for letter in word:
         if letter.lower() in guessed_letters or letter.upper() in guessed_letters:
-            clue += letter + ""
+            clue += letter
+            print(clue)
         else: 
-            clue += "_ "
-        return clue
-    
+            print("____ ")
+               
 while True:
-    print(desplay_clue(word,guessed_letters))
-    guess=input("what is your guessed letter").upper()
+    desplay_clue(word,guessed_letters)
+    guess=input("what is your guessed letter ").upper()
     if guess in guessed_letters:
         print("you've guessed that already")
         #continue   
